3f4aca69bd36c60f56caa7d1b422e448ae060091
b6eful <phoenixrabang@gmail.com>
2020-02-13 10:31:49 -0600

    fixed the models sql and user file

--- db/migrate/20200210194528_modeltable.rb
+++ db/migrate/20200210194528_modeltable.rb
@@ -1,6 +1,6 @@
 class Modeltable < ActiveRecord::Migration[5.2]
   def change
-    create_table :model do |t|
+    create_table :models do |t|
       t.string :stock_name
       t.integer :model_type
     end
